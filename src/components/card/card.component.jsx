82a81cff86fc4eb5041a24c300a3d483f17c6a1e
Raja Nutakky <rnutakky@gmail.com>
2022-02-04 22:59:59 -0500

    adding files for git monster project

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './card.style.css';
+export const Card = (props) => (
+    <div className='card-containers'>
+    <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2`}/>
+     <h1> {props.monster.name}</h1>
+     <p>{props.monster.email}</p>
+    </div>
+)
